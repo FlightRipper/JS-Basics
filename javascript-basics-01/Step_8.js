b51c9eea6dd82ea0229a7ede3fd19ee4e4b93d2e
FlightRipper <101296159+FlightRipper@users.noreply.github.com>
2023-08-30 14:25:30 +0300

    Completed Step_8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,10 @@
+function kousa(){
+    let age = document.getElementById('age');
+    let agevalue = age.value;
+    if (agevalue > 18) {
+        alert("you are over 18");
+    }
+    else {
+        alert("you are under 18");
+    }
+} 
