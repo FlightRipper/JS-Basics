2446352b08bd54c6d40bae94aad5185bfd3f3aea
FlightRipper <101296159+FlightRipper@users.noreply.github.com>
2023-08-30 12:42:13 +0300

    Completed Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,9 @@
+function Kousa(){
+    let first = document.getElementById("first_number");
+    let second = document.getElementById("second_number");
+    let firstvalue = first.value;
+    let secondvalue = second.value;
+    let multiply = secondvalue * firstvalue;
+    alert(multiply)
+
+} 
