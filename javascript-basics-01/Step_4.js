6e1cc19e6d7a900256a8b546bb1ef6a21ce0ec74
FlightRipper <101296159+FlightRipper@users.noreply.github.com>
2023-08-30 11:02:27 +0300

    Completed Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,9 @@
+function Kousa(){
+    let name = document.getElementById("name");
+    let nameValue = name.value;
+    let surname = document.getElementById("surname");
+    let surValue = surname.value;
+    let city = document.getElementById("city"); 
+    let cityValue = city.value;
+    alert("Name : " + nameValue + "\n" + "SurName : " + surValue + "\n" + "City : " + cityValue);
+}
